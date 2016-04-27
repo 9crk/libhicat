f1f0bccc8917913da34c5764bcfb5a9f2d5cfb0f
zhouhua <admin@9crk.com>
2016-04-27 19:04:09 +0800

    add line-track demo

--- app/build.sh
+++ app/build.sh
@@ -1,5 +1,5 @@
 CV_LIBPATH=../opt/opencvlib
-
+arm-hisiv100nptl-linux-g++ lineTrack.cpp ../lib/libhisiv.a -I../include -lpthread -lm -o lineTrack
 
 arm-hisiv100nptl-linux-g++ capture.cpp ../lib/libhisiv.a -I../include -lpthread -lm -o capture -I$CV_LIBPATH/include/opencv -I$CV_LIBPATH/include -L$CV_LIBPATH/lib/ -lopencv_highgui -lopencv_imgproc -lopencv_core -Wno-psabi
 
---
+++ app/lineTrack.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include"libhisiv.h"
+int main(int argc,char* argv[])
+{
+
+	char data[1280*720];
+	int len;
+	int i;
+	len = venc_getYUV(0,data);
+	int x,y;
+	int result[10];
+	
+	for(i=0;i<10;i++){
+		result[i] = 0;
+	}
+	for(i=0;i<10;i++){
+		for(y=635;y<(635+10);y++){
+			for(x=i*72;x<(i+1)*72;i++){
+				result[i]+=data[y*1024+x];
+			}
+		}
+		result[i] = result[i]/(72*10);
+	}
+	for(i=0;i<10;i++){
+		result[i] = 0;
+		printf("result = %d\n",result[i]);
+	}
+}
